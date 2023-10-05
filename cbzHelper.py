f43b3fd313d43c603b93e1eff2c485874521f772
Ali Amin <aamindehkordi@gmail.com>
2023-10-04 23:59:14 -0700

    pdf

--- cbzHelper.py
+++ cbzHelper.py
@@ -1,22 +1,40 @@
-
-# Final complete Python script for converting .cbz files to .epub with specified naming
 import os
 import re
 from zipfile import ZipFile
 from ebooklib import epub
 
-
-# Function to extract volume number and create a simplified name
+# Function to extract volume number and create a simplified name for EPUB
 def generate_epub_name(original_name):
-    # Regular expression to find the volume number in the original file name
     volume_number_match = re.search(r'v(\d+)', original_name)
-
     if volume_number_match:
         volume_number = int(volume_number_match.group(1))
-        return f"OP - Volume {volume_number}.epub"
+        return f"Volume {volume_number}.epub"
     else:
-        return f"OP - {original_name}.epub"
+        return f"One_Piece_{original_name}.epub"
 
+# Additional function to convert CBZ to PDF
+def convert_cbz_to_pdf(cbz_path, pdf_path):
+    from PIL import Image
+    import io
+
+    if not os.path.exists(cbz_path):
+        print(f"Error: {cbz_path} does not exist.")
+        return
+
+    image_list = []
+
+    with ZipFile(cbz_path, 'r') as zip_ref:
+        image_files = sorted([n for n in zip_ref.namelist() if n.lower().endswith(('.png', '.jpg', '.jpeg'))])
+        for img_file in image_files:
+            img_data = zip_ref.read(img_file)
+            img = Image.open(io.BytesIO(img_data))
+            image_list.append(img)
+
+    if image_list:
+        image_list[0].save(pdf_path, save_all=True, append_images=image_list[1:])
+        print(f"Converted {cbz_path} to {pdf_path}.")
+    else:
+        print(f"Error: No images found in {cbz_path}.")
 
 # Function to convert a single CBZ file to EPUB format
 def convert_cbz_to_epub(cbz_path, epub_path):
@@ -62,21 +80,24 @@ def convert_cbz_to_epub(cbz_path, epub_path):
     # Generate EPUB file
     epub.write_epub(epub_path, book)
 
-
-# Main function to convert all CBZ files in a given directory to EPUB files
-def convert_all_cbz_to_epub(folder_path):
+# Main function to convert all CBZ files in a given directory
+def convert_all_cbz(folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith('.cbz'):
             cbz_path = os.path.join(folder_path, filename)
 
-            # Generate the new EPUB file name based on the volume number
-            new_epub_name = generate_epub_name(filename)
-            if new_epub_name is None:
-                print(f"Skipping {filename} as volume number could not be determined.")
-                continue
+            # Convert to EPUB - works :)
+            #new_epub_name = generate_epub_name(filename)
+            #epub_path = os.path.join(folder_path, new_epub_name)
+            #convert_cbz_to_epub(cbz_path, epub_path)
+            #print(f"Converted {filename} to {new_epub_name}.")
 
-            epub_path = os.path.join(folder_path, new_epub_name)
-            convert_cbz_to_epub(cbz_path, epub_path)
-            print(f"Converted {filename} to {new_epub_name}.")
+            # Convert to PDF - Testing
+            new_pdf_name = filename.replace('.cbz', '.pdf')
+            pdf_path = os.path.join(folder_path, new_pdf_name)
+            convert_cbz_to_pdf(cbz_path, pdf_path)
+            print(f"Converted {filename} to {new_pdf_name}.")
 
-convert_all_cbz_to_epub('/Users/aamindehkordi/Downloads/one piece/colored/mana-one-piece-digital-colored-comics')
+if __name__ == "__main__":
+    folder_path = "/Users/aamindehkordi/Downloads/one piece/colored/cbz"
+    convert_all_cbz(folder_path)
